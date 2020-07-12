d82267eb0d2e7bcd1fc83eb37da169bfa87815c2
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2020-07-13 01:29:52 +0200

    Reworked sheet 05

--- sheet05/exercise05_1c.py
+++ sheet05/exercise05_1c.py
@@ -4,59 +4,85 @@ import os
 import random
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.curdir)))
+import sim
 
-from lists import *
+from lists import CellList2D, VerletList
 
-# Exercise 1c)
+
+def get_pos_of_cell(_cell, _positions):
+    _cell_pos = []
+    for _pos_index, pos in enumerate(_positions):
+        if _pos_index in _cell:
+            _cell_pos.append(pos)
+    return _cell_pos
+
+
 if __name__ == "__main__":
-    positions = []
+    sim.domain_lower_bound = 0
+    sim.domain_upper_bound = 1
+    sim.cell_side = 0.05
+    sim.cutoff = 0.05
+
+    particle_pos = []
     random.seed()
 
     for _ in range(0, 10000):
-        positions.append([random.random(), random.random()])
-    positions = np.array(positions)
+        particle_pos.append([random.random(), random.random()])
+    particle_pos = np.array(particle_pos)
 
-    cells = CellList(positions, 0, 1, 0.05)
-    verlet = VerletList(positions, cells, 0.05)
-    position_index = random.randint(0, len(positions))
+    cells = CellList2D(particle_pos)
+    verlet = VerletList(particle_pos, cells)
+    pos_index = random.randint(0, len(particle_pos))
 
-    ## Plotting
-    x = positions[:, 0]
-    y = positions[:, 1]
+    #######################################
+    # Plot particles
+    #######################################
+    x_coords = particle_pos[:, 0]
+    y_coords = particle_pos[:, 1]
 
-    fig, ax = plt.subplots(figsize=(10,10))
-    ## Plot particles and cell grid
-    ax.set_xticks(np.arange(0, cells.upper_bound, cells.cell_width))
-    ax.set_yticks(np.arange(0, cells.upper_bound, cells.cell_width))
-    ax.scatter(x, y, s=10, c="b")
+    fig, ax = plt.subplots()
+    ax.set_xticks(np.arange(sim.domain_lower_bound, sim.domain_upper_bound, sim.cell_side))
+    ax.set_yticks(np.arange(sim.domain_lower_bound, sim.domain_upper_bound, sim.cell_side))
+    ax.scatter(x_coords, y_coords, s=10, c="b")
+    ax.grid()
+    ax.set_title("Neighbours of one particle")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
 
-    ## Plot cell
-    cell_index = random.randint(0, cells.dim_width ** cells.dim_number)
-    print(cell_index)
-    cell_positions = np.array(cells.get_pos_of_cell(positions, cell_index))
-    x = cell_positions[:,0]
-    y = cell_positions[:,1]
-    ax.scatter(x, y, s=10, c="g")
+    #######################################
+    # Plot cell
+    #######################################
+    cell_index = cells.pos_get_cell_index(particle_pos[pos_index])
+    cell_pos = np.array(get_pos_of_cell(cells[cell_index], particle_pos))
 
-    ## Plot neighbouring cells
-    neighbour_positions = []
+    x_coords1 = cell_pos[:, 0]
+    y_coords1 = cell_pos[:, 1]
+    ax.scatter(x_coords1, y_coords1, s=10, c="g")
+
+    #######################################
+    # Plot cell neighbours
+    #######################################
+    cell_neighbour_pos = []
     for neighbour_index in cells.get_neighbour_cell_indices(cell_index):
-        neighbour_positions += cells.get_pos_of_cell(positions, neighbour_index)
-    neighbour_positions = np.array(neighbour_positions)
-    x = neighbour_positions[:,0]
-    y = neighbour_positions[:,1]
-    ax.scatter(x, y, s=10, c="y")
+        cell_neighbour_pos += get_pos_of_cell(cells[neighbour_index], particle_pos)
+    cell_neighbour_pos = np.array(cell_neighbour_pos)
 
-    ## Plot verlet neighbours
-    verlet_neighbours = []
-    for verlet_neighbour_index in verlet.cells[position_index]:
-        verlet_neighbour = positions[verlet_neighbour_index]
-        verlet_neighbours.append(verlet_neighbour)
-    verlet_neighbours = np.array(verlet_neighbours)
-    x = verlet_neighbours[:,0]
-    y = verlet_neighbours[:,1]
-    ax.scatter(x, y, s=10, c="r")
+    x_coords2 = cell_neighbour_pos[:, 0]
+    y_coords2 = cell_neighbour_pos[:, 1]
+    ax.scatter(x_coords2, y_coords2, s=10, c="y", label="Cell neighbours")
 
+    #######################################
+    # Plot verlet neighbours
+    #######################################
+    verlet_neighbour_pos = []
+    for verlet_neighbour_index in verlet.cells[pos_index]:
+        verlet_neighbour = particle_pos[verlet_neighbour_index]
+        verlet_neighbour_pos.append(verlet_neighbour)
+    verlet_neighbour_pos = np.array(verlet_neighbour_pos)
 
-    plt.grid()
-    plt.show()
+    x_coords3 = verlet_neighbour_pos[:, 0]
+    y_coords3 = verlet_neighbour_pos[:, 1]
+    ax.scatter(x_coords3, y_coords3, s=10, c="r", label="Verlet neighbours")
+
+    fig.legend()
+    fig.show()
--- sheet05/exercise05_2a.py
+++ sheet05/exercise05_2a.py
@@ -3,68 +3,101 @@ import matplotlib.pyplot as plt
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.curdir)))
+import sim
 
-from lists import *
+from lists import CellList2D, VerletList
 
-# Exercise 2a)
-if __name__ == "__main__":
-    bacteria_pos = []
-    with open("QSBacterialPos.dat", "r") as f:
+
+def load_bacteria(bacteria_file):
+    _bacteria_pos = []
+    with open(bacteria_file, "r") as f:
         for line in f:
             line = line.replace("\n", "")
-            pos = list(map(lambda x: float(x), line.split("   ")[1:3]))
-            bacteria_pos.append(pos)
-    bacteria_pos = np.array(bacteria_pos)
+            x, y = line.split("   ")[1:3]
+            _bacteria_pos.append((float(x), float(y)))
+    return np.array(_bacteria_pos)
 
-    cells = CellList(bacteria_pos, 0, 10, 0.5)
-    verlet = VerletList(bacteria_pos, cells, 0.5)
 
-    x = bacteria_pos[:,0]
-    y = bacteria_pos[:,1]
+def get_pos_of_cell(_cell, _positions):
+    _cell_pos = []
+    for _pos_index, pos in enumerate(_positions):
+        if _pos_index in _cell:
+            _cell_pos.append(pos)
+    return _cell_pos
 
-    # Left figure (Cell List)
-    fig, ax = plt.subplots()
-    ax.set_xticks(np.arange(0, cells.upper_bound, cells.cell_width))
-    ax.set_yticks(np.arange(0, cells.upper_bound, cells.cell_width))
-    ax.scatter(x, y, s=10, c="b")
-    plt.grid()
 
-    # Cells with high density
+if __name__ == "__main__":
+    sim.domain_lower_bound = 0
+    sim.domain_upper_bound = 10
+    sim.cell_side = 0.5
+    sim.cutoff = 0.5
+
+    bacteria_pos = load_bacteria("QSBacterialPos.dat")
+    cells = CellList2D(bacteria_pos)
+    verlet = VerletList(bacteria_pos, cells)
+
+    x_coords = bacteria_pos[:, 0]
+    y_coords = bacteria_pos[:, 1]
+
+    #######################################
+    # Figure 1: Cells with high density
+    #######################################
     high_density_pos = []
-    for (cell_index, cell) in enumerate(cells.cells):
-        if len(cell) >= 20:
-            high_density_pos += cells.get_pos_of_cell(bacteria_pos, cell_index)
+    for cell_row in cells.cells:
+        for cell in cell_row:
+            if len(cell) >= 20:
+                high_density_pos += get_pos_of_cell(cell, bacteria_pos)
     high_density_pos = np.array(high_density_pos)
-    x1 = high_density_pos[:,0]
-    y1 = high_density_pos[:,1]
-    ax.scatter(x1, y1, s=10, c="r")
+    x_coords1 = high_density_pos[:, 0]
+    y_coords1 = high_density_pos[:, 1]
 
+    fig1, ax1 = plt.subplots()
+    ax1.set_title("Cells with high density")
+    ax1.set_xticks(np.arange(sim.domain_lower_bound, sim.domain_upper_bound, sim.cell_side))
+    ax1.set_yticks(np.arange(sim.domain_lower_bound, sim.domain_upper_bound, sim.cell_side))
+    ax1.scatter(x_coords, y_coords, s=10, c="b")
+    ax1.grid()
 
-    # Right Figure (Verlet)
-    fig, ax = plt.subplots()
-    ax.set_xticks(np.arange(0, cells.upper_bound, cells.cell_width))
-    ax.set_yticks(np.arange(0, cells.upper_bound, cells.cell_width))
-    ax.scatter(x, y, s=10, c="b")
-    plt.grid()
+    ax1.scatter(x_coords1, y_coords1, s=10, c="r")
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
 
-    # Cells with many Vetlet neighbours
-    # Cell with most neighbours
-    high_density_pos = []
+    #######################################
+    # Figure 2: Cells with many neighbours
+    #######################################
+    many_neighbour_pos = []
     most_neighbours = 0
     pos_index_most_neighbours = 0
+
     for (pos_index, cell) in enumerate(verlet.cells):
         neighbours = len(cell)
         if neighbours >= 50:
-            high_density_pos.append(bacteria_pos[pos_index])
+            many_neighbour_pos.append(bacteria_pos[pos_index])
         if neighbours > most_neighbours:
             most_neighbours = neighbours
             pos_index_most_neighbours = pos_index
 
-    high_density_pos = np.array(high_density_pos)
-    x1 = high_density_pos[:,0]
-    y1 = high_density_pos[:,1]
-    ax.scatter(x1, y1, s=10, c="r")
+    many_neighbour_pos = np.array(many_neighbour_pos)
+    x_coords2 = many_neighbour_pos[:, 0]
+    y_coords2 = many_neighbour_pos[:, 1]
 
-    print("The cell with most neighbours is at {} with {} neighbours"\
-            .format(bacteria_pos[pos_index_most_neighbours], most_neighbours))
+    fig2, ax2 = plt.subplots()
+    ax2.set_title("Bacteria with $\geq 50$ neighbours")
+    ax2.set_xticks(np.arange(sim.domain_lower_bound, sim.domain_upper_bound, sim.cell_side))
+    ax2.set_yticks(np.arange(sim.domain_lower_bound, sim.domain_upper_bound, sim.cell_side))
+    ax2.scatter(x_coords, y_coords, s=10, c="b")
+    ax2.grid()
+
+    ax2.scatter(x_coords2, y_coords2, s=10, c="r")
+    ax2.set_xlabel("x")
+    ax2.set_ylabel("y")
     plt.show()
+
+    #######################################
+    # Extra: Bacterium with most neighbours
+    #######################################
+
+    x_most_neighbours, y_most_neighbours = bacteria_pos[pos_index_most_neighbours]
+
+    print("The bacterium with most neighbours is at x={}, y={} with {} neighbours"
+          .format(x_most_neighbours, y_most_neighbours, most_neighbours))
