785fc9529f5351ff890d8999f0381ba5c0de6ec1
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2020-07-13 01:30:03 +0200

    Reworked sheet 02

--- sheet02/exercise02_1c.py
+++ sheet02/exercise02_1c.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import sys
+
+from enum import Enum
 
 c_1 = 1
 c_2 = 1
@@ -8,35 +10,54 @@ x_T = 2 * 10 ** 5
 x_0 = 0.8 * 10 ** 5
 
 
-def euler(x_0, step_size, total_steps, f):
-    x = [x_0]
-    t = [0]
+class Main(Enum):
+    APPROXIMATE_ISOMER_FUNCTION = 0
+    APPROXIMATE_EULER_FUNCTION = 1
 
-    for n in range(0, total_steps):
-        t.append(t[-1] + step_size)
-        x.append(x[-1] + step_size * f(t[-1], x[-1]))
 
-    return np.array(x), np.array(t)
+#######################################
+# Select your main method:
+main = Main.APPROXIMATE_ISOMER_FUNCTION
+#######################################
+
+
+def explicit_euler(_x_0, _dt, _t_max, _f):
+    _x_coords = [_x_0]
+    _t_coords = [0]
+
+    for _ in np.arange(0, _t_max, _dt):
+        _t_coords.append(_t_coords[-1] + dt)
+        _x_coords.append(_x_coords[-1] + dt * f(_t_coords[-1], _x_coords[-1]))
+
+    return np.array(_x_coords), np.array(_t_coords)
 
 
 def f_isomer(t, x_t):
-    global c_1, c_2, x_T, x_0
     return -c_1 * x_t + c_2 * (x_T - x_t)
 
 
-def f_euler(t, x):
-    return x
+def f_euler(t, x_t):
+    return x_t
 
 
 if __name__ == "__main__":
-    # x, t = euler(x_0, 1.0, 4, f_euler)
-    # plt.plot(t, x)
-
     t_max = 10
+    fig, ax = plt.subplots()
 
-    for step_size in np.arange(0.25, 1.5, 0.25):
-        x, t = euler(x_0, step_size, int(t_max / step_size), f_isomer)
-        plt.plot(t, x, label="{}".format(step_size))
+    if main == Main.APPROXIMATE_ISOMER_FUNCTION:
+        f = f_isomer
+        ax.set_title("Approximation of given isomer function")
+    elif main == Main.APPROXIMATE_EULER_FUNCTION:
+        f = f_euler
+        ax.set_title("Approximation of Euler function")
+    else:
+        sys.exit(1)
 
-    plt.legend()
-    plt.show()
+    for dt in np.arange(0.25, 1.5, 0.25):
+        x_coords, t_coords = explicit_euler(x_0, dt, t_max, f)
+        plt.plot(t_coords, x_coords, label="{}".format(dt))
+
+    ax.set_xlabel("t")
+    ax.set_ylabel("x")
+    fig.legend()
+    fig.show()
